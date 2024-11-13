d8d949646461b44b1e044f5c73acda850bd4c3db
peninahd <peninah.deichman@post.runi.ac.il>
2024-11-13 20:20:50 +0200

    Cheers

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,32 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    // gets an input string and N number of times 
+            String word = args[0];
+            int N = Integer.parseInt(args[1]);
+            String anValues = "AEFHILMNORSX";
+            
+            //create a loop that checks each letter and prints it
+            //takes each letter individually and decides if it is 
+            //{'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R','S', 'X'},
+            int i = 0;
+            while (i < word.length()) {
+                char c = word.charAt(i);
+                if (anValues.indexOf(c) == -1) {
+
+                        System.out.println("Give me a " + c + ": " + c + "!");
+                } else {
+
+                        System.out.println("Give me an " + c + ": " + c + "!");
+                }
+                i++;
+            }
+                //print the word N amount of times
+
+                int a = 0;
+                while (a < N){
+                        System.out.println(word + "!!!"); 
+                        a++;
+                }
         }
 }
