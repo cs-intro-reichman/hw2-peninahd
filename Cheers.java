d4e75a2e3c36187ed07e578d18aff958e2259461
peninahd <peninah.deichman@post.runi.ac.il>
2024-11-14 10:20:41 +0200

    upper case issue

--- Cheers.java
+++ Cheers.java
@@ -3,15 +3,16 @@ public class Cheers {
         public static void main(String[] args) {
 	    // gets an input string and N number of times 
             String word = args[0];
+            String wordUpper = word.toUpperCase();
             int N = Integer.parseInt(args[1]);
-            String anValues = "AEFHILMNRSXOoaefhilmnrsx";
+            String anValues = "AEFHILMNRSXO";
             
             //create a loop that checks each letter and prints it
             //takes each letter individually and decides if it is 
             //{'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R','S', 'X'},
             int i = 0;
-            while (i < word.length()) {
-                char c = word.charAt(i);
+            while (i < wordUpper.length()) {
+                char c = wordUpper.charAt(i);
                 if (anValues.indexOf(c) == -1) {
 
                         System.out.println("Give me a  " + c + ": " + c + "!");
@@ -26,7 +27,7 @@ public class Cheers {
                 //print the word N amount of times
                 int a = 0;
                 while (a < N){
-                        System.out.println(word + "!!!"); 
+                        System.out.println(wordUpper + "!!!"); 
                         a++;
                 }
         }
