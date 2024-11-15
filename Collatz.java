aade5a0dae72c376b07546a4e9a9166edd7fd201
peninahd <peninah.deichman@post.runi.ac.il>
2024-11-15 10:20:53 +0200

    Collatz

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,28 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
-}
+	public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        String mode = args[1];
+        int times = 0;
+        boolean firstValue = true;
+        //prints the original N         
+        while (N != 1) {
+            if (!firstValue) {
+                System.out.print(", ");
+            } 
+            System.out.print(N);
+
+            firstValue = false;
+
+                if (N % 2 == 0) {
+                    N = N / 2;
+    
+                } else {
+                    N = (N * 3) + 1;
+                }
+            times++;    
+            }
+        System.out.println(", 1" + " (" + times + ")");
+        System.out.println("Every one of the first " + args[0] + " hailstone sequences reached 1.");
+        }
+    }
